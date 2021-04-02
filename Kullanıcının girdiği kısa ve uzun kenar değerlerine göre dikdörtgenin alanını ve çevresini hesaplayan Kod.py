dcc5ae13dfd9af7058c4adda6d7621794f072b08
KaanPY <77877967+KaanPY@users.noreply.github.com>
2021-04-02 12:10:10 +0300

    Create Kullanıcının girdiği kısa ve uzun kenar değerlerine göre dikdörtgenin alanını ve çevresini hesaplayan Kod.py

---
+++ Kullanıcının girdiği kısa ve uzun kenar değerlerine göre dikdörtgenin alanını ve çevresini hesaplayan Kod.py
@@ -0,0 +1,9 @@
+# Kullanıcının girdiği kısa ve uzun kenar değerlerine göre dikdörtgenin alanını ve çevresini hesaplayınız.
+
+kisaKenar = int(input('Kısa kenar giriniz: '))
+uzunKenar = int(input('Uzun kenar giriniz: '))
+
+alanHesap = (kisaKenar * uzunKenar)
+cevreHesap = ((kisaKenar + uzunKenar)*2)
+
+print(f'\nDiktörgenin Alanı: {alanHesap}\nDikdörtgenin Çevresi: {cevrehesap}')
