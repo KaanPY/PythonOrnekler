df89254daf2c94bce85a1dc8f6f0172c3c316aba
KaanPY <77877967+KaanPY@users.noreply.github.com>
2021-05-27 20:50:23 +0300

    Update Tkinter İle Hesap Makinesi Programı.py

--- Tkinter İle Hesap Makinesi Programı.py
+++ Tkinter İle Hesap Makinesi Programı.py
@@ -44,6 +44,8 @@ def cikis():
 
 win = Tk()
 win.geometry('320x320')
+win.maxsize(320,320)
+win.minsize(320,320)
 win.title('Hesap Makinesi')
 
 number = Entry(foreground='#228b22', font="Courier 14 bold", width=15, justify="right")
