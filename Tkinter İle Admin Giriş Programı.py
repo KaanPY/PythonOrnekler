c5466923b0edea64ced769198fdaf24f9bf78d55
KaanPY <77877967+KaanPY@users.noreply.github.com>
2021-05-30 20:27:42 +0300

    Update Tkinter İle Admin Giriş Programı.py

--- Tkinter İle Admin Giriş Programı.py
+++ Tkinter İle Admin Giriş Programı.py
@@ -5,39 +5,46 @@ import tkinter as tk
 
 def control():
     if((entryText.get() == 'admin')and(entryText2.get() == '123456')):
+        labelText8 = Label(win, text=' ', font='Verdana 10 bold', foreground='green', background='#303030', width=30).place(x=0, y=15)
+        labelText7 = Label(win, text='> Giriş Yapıldı!', font='Verdana 10 bold', foreground='green', background='#303030').place(x=0, y=15)
+        
         entryText.delete(0, 'end')
         entryText2.delete(0, 'end')
-        win2 = Tk()
-        win2.title('Admin Panel!')
-        win2.geometry('250x250')
-        win2.configure(background='#303030')
-        win2.resizable(False, False)
-        win2.wm_attributes('-alpha', 0.9)
+        entryText.focus()
 
-        welcomeAdmin = Label(win2, text='Yapım aşamasında!', font='Verdana 12 bold', foreground='#e63e0b', background='#303030').place(x=40, y=90)
+        win2 = Tk()
+        win2.geometry('320x310')
+        win2.resizable(False, False)
+        win2.configure(background='#303030')
+        win2.wm_attributes('-alpha', 0.8)
+        win2.title('Admin Hk')
+
+        labelText5 = Label(win2, text='Yapım Aşamasında!', font='Verdana 15 bold', foreground='yellow', background='#303030').place(x=47, y=130)
 
         win2.mainloop()
     else:
         entryText2.delete(0, 'end')
-        labelText.configure(text='> Kullanıcı Adı Veya Şifre Yanlış!', font='Verdana 10 bold', foreground='red')
-
+        labelText6 = Label(win, text='> Kullanıcı adı veya şifre yanlış!', font='Verdana 10 bold', foreground='red', background='#303030').place(x=0, y=15)
+        
 win = Tk()
-win.geometry('350x300')
-win.title('Admin Giriş!')
-win.configure(background='#303030')
+win.geometry('320x310+720+320')
 win.resizable(False, False)
+win.configure(background='#303030')
+win.wm_attributes('-alpha', 0.9)
+win.title('Admin Hk Giriş!')
 
-labelText = Label(win, text='Giriş Yapınız!', font='Verdana 15 bold', foreground='orange',backgroun='#303030', width='25', justify='center')
-labelText.place(x=0, y=40)
-text1 = Label(win, text='Kullanıcı Adı:', font='Verdana 10 bold', foreground='#ffffff', background='#303030').place(x=1, y=80)
-text2 = Label(win, text='Şifre:', font='Verdana 10 bold', foreground='#ffffff', background='#303030').place(x=55, y=110)
-text3 = Label(win, text='> Giriş bilgileri; Kullanıcı Adı: admin, Şifre: 123456', font='Verdana 8 bold', foreground='#818386', background='#303030').place(x=0, y=210)
+labelText = Label(win, text='Admin HK', font='Verdana 15 bold', background='#303030', foreground='#ffffff').place(x=100,y=40)
 
-entryText = Entry(win, font='Verdana 10 bold', foreground='#0070b6', width='15', background='#ffffff')
-entryText.place(x=102, y=80)
-entryText2 = Entry(win,show='*', font='Verdana 10 bold', foreground='#0070b6', width='15', background='#ffffff')
-entryText2.place(x=102, y=110)
+labelText2 = Label(win, text='Kullanıcı Adı:', font='Verdana 10 bold', background='#303030', foreground='#ffffff').place(x=110,y=90)
+entryText = Entry(win, font='Verdana 10 bold', width=12, background='#ffffff', foreground='#303030', justify='center')
+entryText.place(x=105,y=115)
+entryText.focus()
+labelText3 = Label(win, text='Şifre:', font='Verdana 10 bold', background='#303030', foreground='#ffffff').place(x=140,y=145)
+entryText2 = Entry(win, show='*', font='Verdana 10 bold', width=12, background='#ffffff', foreground='#303030', justify='center')
+entryText2.place(x=105,y=170)
 
-buttonInput = Button(win, text='Giriş Yap', font='Vedana 10 bold', foreground='#ffffff', width='10', background='#00bb18', command=control).place(x=120, y=140)
+buttonEnter = Button(win, text='Giriş', font='Times 10 italic', foreground='#ffffff', background='#303030', width=10, activebackground='#2e2c2c', activeforeground='green', command=control).place(x=120, y=210)
+
+labelText4 = Label(win, text='> Giriş Bilgileri; Kullanıcı Adı: admin, Şifre: 123456', font='Verdana 7 bold', foreground='#838383', background='#303030').place(x=5, y=260)
 
 win.mainloop()
